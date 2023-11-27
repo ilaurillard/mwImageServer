35b5dc5b3360067925f61c034dd5efb50cd39cff
ilja <ilaurillard@mindworks.de>
2023-11-27 23:20:42 +0100

    markdown

--- lib/MwPdf/Engine/Markdown/Markdown.dart
+++ lib/MwPdf/Engine/Markdown/Markdown.dart
@@ -6,13 +6,23 @@ import 'package:pdf/widgets.dart' as pw;
 class Markdown implements md.NodeVisitor {
   final String markdownContent;
 
-  final double margin1 = 10.0;
-  final double margin2 = 6.0;
-  final double margin3 = 3.0;
-  final double size1 = 12.0;
-  final double size2 = 10.0;
-  final double size3 = 8.0;
-  final double size4 = 6.0;
+  final double margin1 = 8.0 * PdfPageFormat.mm;
+  final double margin2 = 4.0 * PdfPageFormat.mm;
+  final double margin3 = 1.5 * PdfPageFormat.mm;
+  final double size1 = 6.0 * PdfPageFormat.mm;
+  final double size2 = 5.0 * PdfPageFormat.mm;
+  final double size3 = 4.0 * PdfPageFormat.mm;
+  final double size4 = 3.0 * PdfPageFormat.mm;
+  final PdfColor color0 = PdfColors.grey800;
+  final PdfColor color1 = PdfColors.grey200;
+  final PdfColor color2 = PdfColors.grey300;
+  final PdfColor color3 = PdfColors.grey400;
+  final PdfColor color4 = PdfColors.grey500;
+  final PdfColor color5 = PdfColors.grey600;
+  final int bulletIcon = 0xEf4a;
+  final pw.Font monoFont = pw.Font.courier();
+
+  // ---------------------
 
   bool bold = false;
   bool oblique = false;
@@ -23,6 +33,8 @@ class Markdown implements md.NodeVisitor {
   List<pw.TextSpan> spans = [];
 
   int listDepth = 0;
+  List<int> listNr = [];
+  int quoteDepth = 0;
 
   Markdown(
     this.markdownContent,
@@ -38,40 +50,59 @@ class Markdown implements md.NodeVisitor {
 
   // ----------------------------
 
-  void pre() {
-    List<pw.Widget> children = widgets.removeLast();
-
-    widgets.last.add(
-      pw.Container(
-        margin: pw.EdgeInsets.only(bottom: margin1),
-        child: pw.Row(
-          crossAxisAlignment: pw.CrossAxisAlignment.start,
-          children: [
-            pw.Expanded(
-              child: pw.Container(
-                padding: pw.EdgeInsets.all(
-                  margin1,
-                ),
-                decoration: pw.BoxDecoration(
-                  color: PdfColors.grey200,
-                ),
-                child: pw.Column(
-                  crossAxisAlignment: pw.CrossAxisAlignment.start,
-                  children: children,
-                ),
-              ),
+  void headline() {
+    if (spans.isNotEmpty) {
+      widgets.last.add(
+        pw.Container(
+          padding: pw.EdgeInsets.only(bottom: margin1),
+          child: pw.RichText(
+            overflow: pw.TextOverflow.span,
+            text: pw.TextSpan(
+              children: spans,
             ),
-          ],
+          ),
         ),
-      ),
-    );
-    spans = [];
+      );
+      spans = [];
+    }
+  }
+
+  void paragraph() {
+    if (spans.isNotEmpty) {
+      widgets.last.add(
+        pw.Container(
+          padding: pw.EdgeInsets.only(bottom: margin1),
+          child: pw.RichText(
+            overflow: pw.TextOverflow.span,
+            text: pw.TextSpan(
+              children: spans,
+            ),
+          ),
+        ),
+      );
+      spans = [];
+    }
   }
 
   void quote() {
-    // print('  >>> XXX ${widgets.length}');
-
     List<pw.Widget> children = widgets.removeLast();
+
+    PdfColor c = color5;
+    switch (quoteDepth) {
+      case 0:
+        c = color1;
+        break;
+      case 1:
+        c = color2;
+        break;
+      case 2:
+        c = color3;
+        break;
+      case 3:
+        c = color4;
+        break;
+    }
+
     widgets.last.add(
       pw.Container(
         margin: pw.EdgeInsets.only(bottom: margin1),
@@ -83,15 +114,16 @@ class Markdown implements md.NodeVisitor {
               child: pw.Container(
                 padding: pw.EdgeInsets.only(
                   left: margin1,
-                  top: margin1,
+                  top: margin2,
                   right: margin1,
                   bottom: margin2,
                 ),
                 decoration: pw.BoxDecoration(
-                  color: PdfColors.grey300,
+                  color: c,
                   border: pw.Border(
                     left: pw.BorderSide(
-                      color: PdfColors.grey,
+                      // style: pw.BorderStyle.dashed,
+                      color: color0,
                       width: margin3,
                     ),
                   ),
@@ -113,9 +145,9 @@ class Markdown implements md.NodeVisitor {
     List<pw.Widget> children = widgets.removeLast();
     widgets.last.add(
       pw.Container(
-        // color: PdfColors.orange,
         padding: pw.EdgeInsets.only(
-          bottom: listDepth == 0 ? margin1 : 0,
+          top: listDepth == 0 ? 0 : margin3,
+          bottom: listDepth == 0 ? margin1 : margin3,
         ),
         child: pw.Column(
           children: children,
@@ -133,15 +165,23 @@ class Markdown implements md.NodeVisitor {
         children: [
           pw.Padding(
             padding: pw.EdgeInsets.only(
+              left: margin3,
+              top: listNr.last == 0 ? size3 / 2 : 0,
               right: margin2,
-              top: 6,
             ),
-            child: pw.Icon(
-              pw.IconData(
-                0xEf4a,
-              ),
-              size: 8,
-            ),
+            child: listNr.last > 0
+                ? pw.Text(
+                    '${listNr.last}.',
+                    // style: pw.TextStyle(
+                    //     fontWeight: pw.FontWeight.bold,
+                    //     ),
+                  )
+                : pw.Icon(
+                    pw.IconData(
+                      bulletIcon,
+                    ),
+                    size: size3,
+                  ),
           ),
           pw.Expanded(
             child: pw.Column(
@@ -152,63 +192,84 @@ class Markdown implements md.NodeVisitor {
         ],
       ),
     );
-  }
-
-  void paragraph() {
-    if (spans.isNotEmpty) {
-      widgets.last.add(
-        pw.Container(
-          // color: PdfColors.lightBlue,
-          padding: pw.EdgeInsets.only(bottom: margin1),
-          child: pw.RichText(
-            overflow: pw.TextOverflow.span,
-            text: pw.TextSpan(
-              children: spans,
-            ),
-          ),
-        ),
-      );
-      spans = [];
+    if (listNr.last > 0) {
+      listNr.last++;
     }
   }
 
-  void headline() {
-    if (spans.isNotEmpty) {
-      widgets.last.add(
-        pw.Container(
-          // color: PdfColors.lightGreen,
-          padding: pw.EdgeInsets.only(bottom: margin1),
-          child: pw.RichText(
-            overflow: pw.TextOverflow.span,
-            text: pw.TextSpan(
-              children: spans,
+  void pre() {
+    List<pw.Widget> children = widgets.removeLast();
+
+    widgets.last.add(
+      pw.Container(
+        margin: pw.EdgeInsets.only(bottom: margin1),
+        child: pw.Row(
+          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          children: [
+            pw.Expanded(
+              child: pw.Container(
+                padding: pw.EdgeInsets.all(
+                  margin1,
+                ),
+                decoration: pw.BoxDecoration(
+                  color: color1,
+                ),
+                child: pw.Column(
+                  crossAxisAlignment: pw.CrossAxisAlignment.start,
+                  children: children,
+                ),
+              ),
             ),
-          ),
+          ],
         ),
-      );
-      spans = [];
-    }
+      ),
+    );
+    spans = [];
   }
 
   // -----------------------
+
+  void parse(
+    String markdownContent,
+  ) {
+    spans = [];
+    widgets = [[]];
+    for (md.Node node in md.Document(
+      encodeHtml: false,
+    ).parseLines(
+      markdownContent.split('\n'),
+    )) {
+      node.accept(this);
+    }
+  }
 
   @override
   bool visitElementBefore(
     md.Element element,
   ) {
-    print('start ${element.tag}');
+    // print('start ${element.tag}');
 
     switch (element.tag) {
       case 'ul':
+        listDepth++;
+        spansToRichText();
+        widgets.add([]);
+        listNr.add(0);
+        break;
       case 'ol':
         listDepth++;
-        consumeSpans();
+        spansToRichText();
         widgets.add([]);
+        listNr.add(1);
         break;
       case 'blockquote':
+        quoteDepth++;
+        spansToRichText();
+        widgets.add([]);
+        break;
       case 'pre':
       case 'li':
-        consumeSpans();
+        spansToRichText();
         widgets.add([]);
         break;
       case 'h1':
@@ -247,7 +308,7 @@ class Markdown implements md.NodeVisitor {
   void visitElementAfter(
     md.Element element,
   ) {
-    print('end ${element.tag}');
+    // print('end ${element.tag}');
 
     switch (element.tag) {
       case 'p':
@@ -264,14 +325,15 @@ class Markdown implements md.NodeVisitor {
         bold = false;
         break;
       case 'blockquote':
+        quoteDepth--;
         quote();
         break;
       case 'pre':
-        consumeSpans();
+        spansToRichText();
         pre();
         break;
       case 'li':
-        consumeSpans();
+        spansToRichText();
         listElement();
         size = null;
         bold = false;
@@ -280,6 +342,7 @@ class Markdown implements md.NodeVisitor {
       case 'ol':
         listDepth--;
         list();
+        listNr.removeLast();
         break;
       case 'em':
         oblique = false;
@@ -309,9 +372,9 @@ class Markdown implements md.NodeVisitor {
     if (code || bold || oblique || size != null) {
       if (code) {
         bg = pw.BoxDecoration(
-          color: PdfColors.grey200,
+          color: color1,
         );
-        font = pw.Font.courier();
+        font = monoFont;
       }
 
       textStyle = pw.TextStyle(
@@ -331,21 +394,7 @@ class Markdown implements md.NodeVisitor {
     );
   }
 
-  void parse(
-    String markdownContent,
-  ) {
-    spans = [];
-    widgets = [[]];
-    for (md.Node node in md.Document(
-      encodeHtml: false,
-    ).parseLines(
-      markdownContent.split('\n'),
-    )) {
-      node.accept(this);
-    }
-  }
-
-  void consumeSpans() {
+  void spansToRichText() {
     if (spans.isNotEmpty) {
       widgets.last.add(
         pw.RichText(
